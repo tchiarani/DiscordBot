ebf4764010475978e8dfe03f4de4db8fe8dceef4
Thomas <44852062+tchiarani@users.noreply.github.com>
2020-01-27 16:24:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ let queue = [];
 servers.forEach(id => initGuild(id))
 
 console.log(client.guilds.values)
+console.log(client.guilds.values())
 console.log(Array.from(client.guilds.values()))
 console.log(Array.from(client.guilds))
 
