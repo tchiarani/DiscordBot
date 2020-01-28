2641b38eaa686a3db025045683323cd00bc0f493
tchiarani <tchiarani@gmail.com>
2020-01-28 20:00:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -89,9 +89,10 @@ function end(connection, message, action) {
         data[message.guild.id]['dataVideoEmbed'] = []
     }
     if (data[message.guild.id]['queue'].length == 0) {
-        message.channel.send("Déconnexion du salon " + connection.channel.name).then(function(msg) {
-            setTimeout(msg.delete, 5000)
-        }).catch(console.log())
+        message.channel.send("Déconnexion du salon " + connection.channel.name)
+            .then(function(msg) {
+                setTimeout(msg.delete, 5000)
+            }).catch(console.log())
         connection.disconnect()
     } else {
         play(connection, message, 'Skip')
