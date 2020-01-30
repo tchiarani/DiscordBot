441442c0348a69cc3a5f07d20001aa40f8f40629
tchiarani <tchiarani@gmail.com>
2020-01-30 19:11:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -440,7 +440,7 @@ function setMusicEmbed(id, video) {
             .setTitle(video.title)
             .setDescription("Durée : " + video.timestamp)
             .setAuthor(video.author.name, "https://i.imgur.com/MBNSqyF.png", "https://youtube.com/channel/" + video.author.id)
-            .setThumbnail("https://img.youtube.com/vi/" + video.videoId + "/mqdefault.jpg")
+            .setThumbnail("https://img.youtube.com/vi/" + video.videoId + "/mqdefault.jpg", 50, 50)
             .setColor('#FF0000')
             .setURL("https://youtube.com" + video.url)
         )
