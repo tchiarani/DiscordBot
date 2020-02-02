47630301f0c52cc939967e7b1c656a53711b7be5
tchiarani <tchiarani@gmail.com>
2020-02-02 18:53:49 +0100

    Update index.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,6 @@ module.exports = {
     aliases: ['commandes', 'commands'],
     usage: '[commande]',
     execute(message, args) {
-        const data = []
         const { commands } = message.client
 
         if (!args.length) {
@@ -18,23 +17,9 @@ module.exports = {
                 .setAuthor("Besoin d'aide ?", config.botAvatar, "https://unikorn.ga/bot")
                 .setColor('#7289DA')
                 .setFooter("unikorn.ga | /help", config.authorAvatar)
-                .addField("----------------", config.prefix + commands.map(command => command.name).slice(0, (commands.length + 1) / 2).join("\n" + config.prefix), true)
-                .addField("----------------", config.prefix + commands.map(command => command.name).slice((commands.length + 1) / 2, commands.length).join("\n" + config.prefix), true)
+                .addField("----------------", config.prefix + commands.map(command => command.name).slice(0, (command.length + 1) / 2).join("\n" + config.prefix), true)
+                .addField("----------------", config.prefix + commands.map(command => command.name).slice((command.length + 1) / 2, command.length).join("\n" + config.prefix), true)
             message.channel.send(dataHelp)
-
-            data.push('Here\'s a list of all my commands:')
-            data.push(commands.map(command => command.name).join(', '))
-            data.push(`\nYou can send \`${config.prefix}help [command name]\` to get info on a specific command!`)
-
-            return message.author.send(data, { split: true })
-                .then(() => {
-                    if (message.channel.type === 'dm') return
-                    message.reply('I\'ve sent you a DM with all my commands!')
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error)
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?')
-                })
 
         } else {
             const name = args[0].toLowerCase()
