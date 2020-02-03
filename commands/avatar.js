33f404c364c87223a88a28e372430df374326c97
tchiaran <tchiaran@BE19-14-L.sfasp2mi.univ-poitiers.fr>
2020-02-03 16:27:02 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -14,9 +14,9 @@ module.exports = {
         } else {
             avatar = await Canvas.loadImage(message.author.displayAvatarURL)
         }
-        const canvas = Canvas.createCanvas(500, 500)
+        const canvas = Canvas.createCanvas(256, 256)
         const ctx = canvas.getContext("2d")
-        ctx.drawImage(avatar, 0, 0) //, canvas.width, canvas.height)
+        ctx.drawImage(avatar, 0, 0, canvas.width, canvas.height)
         const attachment = new Discord.Attachment(canvas.toBuffer(), "userAvatar.png")
         message.channel.send(attachment)
     }
