d0418dc8a2849aff138e064b10ba65b10282f4b9
tchiaran <tchiaran@BE19-14-L.sfasp2mi.univ-poitiers.fr>
2020-02-03 16:05:45 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -10,18 +10,18 @@ module.exports = {
         let background
         if (message.mentions.users.size) {
             const taggedUser = message.mentions.users.first()
-            console.log(taggedUser.displayAvatarURL.height)
-            console.log(taggedUser.displayAvatarURL.size)
             background = await Canvas.loadImage(taggedUser.displayAvatarURL)
         } else {
             background = await Canvas.loadImage(message.author.displayAvatarURL)
-            console.log(message.author.displayAvatarURL)
-            console.log(message.author.avatarURL)
         }
         const canvas = Canvas.createCanvas(500, 500)
         const ctx = canvas.getContext("2d")
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
         const attachment = new Discord.Attachment(canvas.toBuffer(), "userAvatar.png")
         message.channel.send(attachment)
+
+        var img = new Image()
+        img.src = message.author.displayAvatarURL
+        console.log(img.width + ' ' + img.height)
     }
 }
