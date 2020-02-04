e6bebda57dabbf4206bdeca571243dd3f19f1c47
tchiaran <tchiaran@BE19-13-L.sfasp2mi.univ-poitiers.fr>
2020-02-04 12:01:36 +0100

    Update play.js for youtube playlist

--- commands/play.js
+++ commands/play.js
@@ -46,17 +46,16 @@ module.exports = {
                         let regExp = /^.*(youtu.be\/|list=)([^#\&\?]*).*/
                         if (args[0] > 0 && args[0] < data[message.guild.id]['queue'].length) {
                             if (data[message.guild.id]['queue'][args[0]]) {
-                                data[message.guild.id]['musicTitle'].splice(1, 0, data[message.guild.id]['musicTitle'][args[0]]).splice(args[0], 1)
-                                data[message.guild.id]['musicDuration'].splice(1, 0, data[message.guild.id]['musicDuration'][args[0]]).splice(args[0], 1)
-                                data[message.guild.id]['queue'].splice(1, 0, data[message.guild.id]['queue'][args[0]]).splice(args[0], 1)
-                                data[message.guild.id]['dataQueue'].splice(1, 0, data[message.guild.id]['dataQueue'][args[0]]).splice(args[0], 1)
-                                    .then(client.commands.get("skip").execute(client, message, args, data))
+                                data[message.guild.id]['musicTitle'].splice(1, 0, data[message.guild.id]['musicTitle'][args[0]]).splice(args[0], 1).then(
+                                    data[message.guild.id]['musicDuration'].splice(1, 0, data[message.guild.id]['musicDuration'][args[0]]).splice(args[0], 1)).then(
+                                    data[message.guild.id]['queue'].splice(1, 0, data[message.guild.id]['queue'][args[0]]).splice(args[0], 1)).then(
+                                    data[message.guild.id]['dataQueue'].splice(1, 0, data[message.guild.id]['dataQueue'][args[0]]).splice(args[0], 1)).then(
+                                    client.commands.get("skip").execute(client, message, args, data))
                             }
                         } else if (args[0].match(regExp)) {
                             if (ytpl.validateURL(args[0].match(regExp)[2])) {
                                 ytpl(args[0].match(regExp)[2], { limit: Infinity }, function(err, playlist) {
                                     if (err) return console.log(err)
-                                    console.log(typeof playlist)
                                     if (typeof playlist != undefined) {
                                         data[message.guild.id]['firstResult'] = playlist
                                         for (let i = 0; i < playlist.items.length; i++) {
