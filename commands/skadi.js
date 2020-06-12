53807cd1c90ca5270a43387a849b68be857c1871
tchiarani <tchiarani@gmail.com>
2020-06-12 19:32:28 +0200

    Modify Skadi command

--- commands/skadi.js
+++ commands/skadi.js
@@ -11,8 +11,8 @@ module.exports = {
         axios.get('https://trello.com/b/Tq88EA4I.json')
             .then(function(response) {
                 console.log(response);
-                console.log(response.data.name);
-                console.log(response.data.lists);
+                console.log(response.data.lists.limits);
+                console.log(response.data.lists.limits.cards);
             })
             .catch(function(error) {
                 console.log(error);
