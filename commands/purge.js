950f3de7b7db5f8ed438e2d6cc59abda0566f630
Thomas <44852062+tchiarani@users.noreply.github.com>
2020-08-09 20:22:52 +0200

    style(purge): Remove delete message

--- commands/purge.js
+++ commands/purge.js
@@ -7,10 +7,9 @@ module.exports = {
         if (args[0] == undefined || args[0] < 1 || args[0] > 100) {
             message.reply('La valeur doit être comprise entre 1 et 100.')
         } else {
-            message.delete()
             message.channel.bulkDelete(args[0])
                 .then(() => { 
-                    message.channel.send("Les args[0] derniers messages viennent d'être supprimés.").then(msg => msg.delete(3000))
+                    message.delete()
                 })
                 .catch(console.error)
         }
